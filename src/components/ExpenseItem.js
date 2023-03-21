d78149da929e3884cf695f1407f8e111a230b7e4
juhi-coder <kumarijuhimdb2@gmail.com>
2023-03-21 16:52:50 +0530

    FirstReact_App

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItems(){
+return (<div>
+    <h2>Expense Items</h2>
+    <div>Food Rs 10</div>
+    <div>Petrol Rs 100</div>
+    <div>Movies Rs 200</div>
+</div>)
+}
+export default ExpenseItems;
